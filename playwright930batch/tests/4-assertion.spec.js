77f0372662e89964a2f188227f77645576696f01
Dipanshu-26 <dnchawde@gmail.com>
2026-09-10 22:08:35 +0530

    10Sept

---
+++ playwright930batch/tests/4-assertion.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+import { escape } from "node:querystring";
+
+test("assertion types",async({page})=>{
+    //AAA
+    await page.goto("https://www.example.com/")
+
+    await expect(page).toHaveTitle("Example Domain")
+
+    await page.waitForTimeout(3000)
+
+    const heading =await page.locator('h1')
+    let hd = await page.locator('h1').textContent()
+    console.log(hd)
+    console.log(heading)
+
+    await expect(heading).toBeVisible()
+    await expect(heading).toHaveText("Example Domain")
+
+    await expect(heading).toContainText("Example")
+
+    let para= await page.locator('p')
+    await expect(para).toHaveCount(2)
+
+    
+    await expect(heading).toHaveCount(1)
+
+    const link = await page.locator('a')
+    await expect(link).toBeEnabled()
+
+    await expect(link).toHaveAttribute('href',"https://iana.org/domains/example")
+})
