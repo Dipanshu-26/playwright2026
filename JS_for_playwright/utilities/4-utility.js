40eab63f498abe4b98a3c2cc6877cc4bd35ab065
Dipanshu-26 <dnchawde@gmail.com>
2026-09-16 21:58:21 +0530

    16Sept

---
+++ JS_for_playwright/utilities/4-utility.js
@@ -0,0 +1,48 @@
+class MathUtility2 {
+    static username = "dipanshu"
+    static password = "dip"
+    static marks = [80, 90, 70]
+
+    static add(a, b) {
+        console.log(a + b)
+    }
+
+    static sub(a, b) {
+        console.log(a - b)
+    }
+
+    static mult(a, b) {
+        console.log(a * b)
+    }
+
+    static div(a, b) {
+        console.log(a / b)
+    }
+
+    static mod(a, b) {
+        console.log(a % b)
+    }
+
+    static pow(a, b) {
+        console.log(a ** b)
+    }
+
+    static sqrt(a) {
+        console.log(Math.sqrt(a))
+    }
+
+    static log(a) {
+        console.log(Math.log(a))
+    }
+
+    static sin(a) {
+        console.log(Math.sin(a))
+    }
+
+    static cos(a) {
+        console.log(Math.cos(a))
+
+    }
+}
+
+export default MathUtility2
--- JS_for_playwright/utilities/scripts.js
+++ JS_for_playwright/utilities/scripts.js
@@ -16,8 +16,18 @@
 //---------------------------------------------------------------------------------------------
 //3-utility.js
 
-import mu from "../utilities/3-utility.js"
+// import mu from "../utilities/3-utility.js"
 
-const obj=new mu("pune","india")
-obj.add(100,200)
-obj.mul(20,5)
+// const obj=new mu("pune","india")
+// obj.add(100,200)
+// obj.mul(20,5)
+
+//---------------------------------------------------------------------------------------------
+
+//4-utility.js
+import mathUtil from "../utilities/4-utility.js"
+
+mathUtil.sub(100,200)
+mathUtil.mult(20,5)
+mathUtil.div(100,20)
+
